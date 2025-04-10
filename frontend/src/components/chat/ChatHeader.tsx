d8c50318ee88821a8e685183f8f1bd2b5e23eaed
SoumyBhow <soumybhow1.0@gmail.com>
2025-04-09 22:43:11 -0400

    Minor tweaks

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -1,18 +1,8 @@
-import { useNavigate } from 'react-router';
-
 export default function ChatHeader() {
-  const navigate = useNavigate();
-  const handleTitleClick = () => {
-    navigate('/');
-  };
-
   return (
     <div className="flex items-center justify-between p-4 bg-[#2D2374]">
-      <h2
-        className="text-lg text-white cursor-pointer"
-        onClick={handleTitleClick} // Added onClick to make the title clickable
-      >
-        <span className="font-bold">MOMEMTUM</span> ASSISTANT
+      <h2 className="text-lg text-white">
+        <span className="font-bold ">MOMEMTUM</span> ASSISTANT
       </h2>
     </div>
   );
