7221d3cb911d1aa862b360a88894d6e28176ccf3
SoumyBhow <soumybhow1.0@gmail.com>
2025-04-10 17:32:40 -0400

    Reduce sidebar width

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -124,12 +124,12 @@ export default function ChatPage() {
       {/* Main Content */}
       <div className="flex flex-row flex-grow min-h-0">
         {/* Sidebar */}
-        <div className="w-1/4 bg-primary-normal">
+        <div className="w-1/6 bg-primary-normal">
           <ChatSideBar />
         </div>
 
         {/* Chat Area */}
-        <div className="flex flex-col w-3/4">
+        <div className="flex flex-col w-5/6">
           {/* Chat Bubbles */}
           <div className="flex-grow p-4 bg-gray-100 border-t border-gray-300 overflow-y-auto">
             <ChatBubble content="Hello, how are you?" isUser={true} />
